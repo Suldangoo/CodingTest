02d9f2ed6d563359db363f0d053551b24e7fdc61
JunHyeong Park <suldangoo@gmail.com>
2023-03-29 20:16:44 +0900

    comiit

--- baekjoon/gold/1107. 리모컨/리모컨.py
+++ baekjoon/gold/1107. 리모컨/리모컨.py
@@ -1,3 +1,26 @@
 # 1107. 리모컨 (골드 5)
 # 알고리즘 분류 : 브루트포스
 
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+m = int(sys.stdin.readline().rstrip())
+broken = []
+if m > 0 :
+    broken = list(map(int, sys.stdin.readline().rstrip().split()))
+
+ans = abs(n - 100)
+
+for i in range(n, 500001) :
+    for j in str(i) :
+        if not j in broken :
+            ans = ans if ans < len(i) else len(i)
+        ans = ans if ans < n - 100 else n - 100
+
+for i in range(0, n) :
+    for j in str(i) :
+        if not j in broken :
+            ans = ans if ans < len(i) else len(i)
+        ans = ans if ans < n - 100 else n - 100
+
+print(ans)
