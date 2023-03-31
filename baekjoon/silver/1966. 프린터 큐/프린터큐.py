549c083dc93b1e26221673414c581ebf973efeb8
JunHyeong Park <suldangoo@gmail.com>
2023-03-31 16:49:38 +0900

    commit

--- baekjoon/silver/1966. 프린터 큐/프린터큐.py
+++ baekjoon/silver/1966. 프린터 큐/프린터큐.py
@@ -10,24 +10,24 @@ for _ in range(test) :
     n, m = map(int, sys.stdin.readline().split())
     importance = list(map(int, sys.stdin.readline().split()))
 
-    queue = deque(importance)
-    importance.sort()
+    queue = deque(importance) # 중요도를 그대로 살려 큐 생성
+    importance.sort() # 중요도를 오름차순으로 정렬
 
-    i = 1
-
+    i = 1 # 지금 출력한 종이가 몇 번째로 출력한 종이인지 체크할 변수
+    # 리스트의 길이가 0이 될 때까지 반복
     while(len(importance) > 0) :
-        if queue[0] == importance[-1] :
-            del importance[-1]
-            queue.popleft()
-            if m == 0 :
-                print(i)
+        if queue[0] == importance[-1] : # 만약 지금 제일 앞에 있는 종이가 가장 중요도가 높을 경우
+            del importance[-1] # 가장 높은 중요도 리스트 하나 제거
+            queue.popleft() # 종이 출력
+            if m == 0 : # 만약 이번 종이가 추적중인 종이었다면
+                print(i) # i 출력 후 break
                 break
-            else :
-                i += 1
-                m -= 1
+            else : # 추적중인 종이가 아니라면
+                i += 1 # 출력한 종이 갯수 증가
+                m -= 1 # 현재 추적중인 종이 순서 갱신
         else :
-            queue.append(queue.popleft())
-            if (m == 0) :
-                m = len(importance) - 1
+            queue.append(queue.popleft()) # 중요도가 더 높은게 있다면 가장 뒤로 빼버리기
+            if (m == 0) : # 그 종이가 추적중인 종이었다면
+                m = len(importance) - 1 # 가장 뒤로 보내버리기
             else :
-                m -= 1
+                m -= 1 # 아니라면 추적중인 종이 순서 갱신
