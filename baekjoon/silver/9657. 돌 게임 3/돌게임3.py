b6c4153334c447f68bd19f112bf0f47d9016625e
JunHyeong Park <suldangoo@gmail.com>
2023-04-25 23:38:32 +0900

    comit

--- baekjoon/silver/9657. 돌 게임 3/돌게임3.py
+++ baekjoon/silver/9657. 돌 게임 3/돌게임3.py
@@ -2,9 +2,12 @@
 # 알고리즘 분류 : 다이나믹 프로그래밍, 게임 이론
 
 n = int(input())
+# 상근이가 이기는 경우 1
+# 찬영이가 이기는 경우 0
 winner = [1, 0, 1, 1]
 
 for i in range(5, 1001) :
+    # 돌의 개수보다 1개, 3개, 4개 적은 걸 모두 이겼었다면, 해당 돌의 개수는 상대방이 무조건 이김
     if winner[i - 2] + winner[i - 4] + winner[i - 5] < 3 :
         winner.append(1)
     else :
