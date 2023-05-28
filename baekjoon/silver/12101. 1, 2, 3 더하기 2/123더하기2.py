e1d83011b1b8ab9054d63489850045bbd3c3af24
suldangoo <suldangoo@gmail.com>
2023-05-28 23:45:25 +0900

    commit

--- baekjoon/silver/12101. 1, 2, 3 더하기 2/123더하기2.py
+++ baekjoon/silver/12101. 1, 2, 3 더하기 2/123더하기2.py
@@ -1,27 +1,30 @@
 # 12101. 1, 2, 3 더하기 2 (실버 1)
 # 알고리즘 분류 : 브루트포스 알고리즘, 백트래킹
 
+def dfs(nums, sumation, target):
+    # 타겟의 숫자를 만들기 위해 nums에 1, 2, 3을 넣으며 재귀를 돌림
+    global case # 전역변수 리스트 case를 사용
+
+    # 현재 누적값이 목표치와 같으면 리스트 추가 후 리턴
+    if sumation == target:
+        case.append(nums)
+        return
+    
+    # 현재 누적값이 목표치를 넘겨버렸다면 리턴
+    if sumation > target:
+        return
+
+    # 1, 2, 3을 넣은 리스트를 생성함과 동시에 누적값을 다음 재귀로 보냄
+    for i in [1, 2, 3]:
+        dfs(nums + [i], sumation + i, target)
+
 n, k = map(int, input().split())
+case = []
 
-max_three = n // 3
-numlists = []
+dfs([], 0, n)
 
-for i in range(max_three + 1) :
-    numlist = []
-    num = n - i * 3
-    max_two = num // 2
-    for j in range(max_two + 1) :
-        num = n - i * 3
-
-        for _ in range(i) :
-            numlist.append(3)
-        for _ in range(j) :
-            numlist.append(2)
-        
-        while True :
-            if num <= 0 :
-                break
-            else :
-                num -= 1
-                numlist.append(1)
-    print(numlist)
+# k - 1번째 인덱스 호출
+if k - 1 < len(case):
+    print("+".join(map(str, case[k - 1])))
+else:
+    print(-1)
