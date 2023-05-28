98a5d517146948ecd214759163a369dc79c320b5
suldangoo <suldangoo@gmail.com>
2023-05-28 23:48:12 +0900

    commit

--- baekjoon/silver/12101. 1, 2, 3 더하기 2/123더하기2.py
+++ baekjoon/silver/12101. 1, 2, 3 더하기 2/123더하기2.py
@@ -1,22 +1,23 @@
 # 12101. 1, 2, 3 더하기 2 (실버 1)
 # 알고리즘 분류 : 브루트포스 알고리즘, 백트래킹
 
-def dfs(nums, sumation, target):
+def dfs(nums, accumulate, target):
     # 타겟의 숫자를 만들기 위해 nums에 1, 2, 3을 넣으며 재귀를 돌림
     global case # 전역변수 리스트 case를 사용
 
     # 현재 누적값이 목표치와 같으면 리스트 추가 후 리턴
-    if sumation == target:
+    if accumulate == target:
         case.append(nums)
         return
     
     # 현재 누적값이 목표치를 넘겨버렸다면 리턴
-    if sumation > target:
+    if accumulate > target:
         return
 
     # 1, 2, 3을 넣은 리스트를 생성함과 동시에 누적값을 다음 재귀로 보냄
+    # 1 - 1 - 1... 순서부터 시작하므로 정렬해줄 필요가 없음
     for i in [1, 2, 3]:
-        dfs(nums + [i], sumation + i, target)
+        dfs(nums + [i], accumulate + i, target)
 
 n, k = map(int, input().split())
 case = []
