2e4571c49cb728bde17cf13c06c1b9667dba759b
suldangoo <suldangoo@gmail.com>
2023-09-20 16:47:00 +0900

    commit

---
+++ baekjoon/silver/1874. 스택 수열/스택수열.py
@@ -0,0 +1,27 @@
+# 1874. 스택 수열 (실버 2)
+# 알고리즘 분류 : 자료 구조, 스택
+
+n = int(input())
+nums = []
+
+for _ in range(n) :
+    nums.append(int(input())) # 사용자로부터 수열을 입력받아 nums에 추가
+
+answer = [] # +와 -가 담길 최종 답 리스트
+stack = [] # 풀이에 쓰일 스택 리스트
+numlist = list(range(1, n + 1)) # 1부터 n까지의 숫자가 담겨있는 팔레트
+
+# 입력한 수열과 숫자 하나씩 비교
+for i in nums :
+    while i not in stack : # 만약 스택에 수열의 숫자가 업다면
+        answer.append('+') # 숫자 팔레트에서 push로 스택에 숫자를 가져오기
+        stack.append(numlist.pop(0))
+
+    if stack[-1] == i : # 스택의 가장 마지막 수가 수열의 숫자라면
+        answer.append('-') # 스택에서 pop으로 숫자 출력
+        stack.pop()
+    else :
+        print("NO") # 아니라면 수열을 완성할 수 없으므로 NO로 종료
+        exit(0)
+
+print(*answer, sep='\n') # 정답 문자열을 전부 출력
