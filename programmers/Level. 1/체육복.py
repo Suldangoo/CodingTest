4f649b71e04093ab47093945eee4af547a7bc2b0
JunHyeong Park <suldangoo@gmail.com>
2024-01-29 00:53:37 +0900

    commit

--- programmers/Level. 1/체육복.py
+++ programmers/Level. 1/체육복.py
@@ -13,6 +13,7 @@ def solution(n, lost, reserve):
         if i + 1 in reserve :
             team[i] += 1
             
+    # 배열 바깥 처리를 위한 예외 처리
     if team[0] == 2 and team[1] == 0 :
         team[0] -= 1
         team[1] += 1
@@ -21,6 +22,7 @@ def solution(n, lost, reserve):
         team[n-1] -= 1
         team[n-2] += 1
 
+    # 좌우에 비어있는 사람이 있다면, 체육복 대여
     for i in range(1, n-1) :
         if team[i] == 2 and team[i - 1] == 0 :
             team[i] -= 1
@@ -29,6 +31,7 @@ def solution(n, lost, reserve):
             team[i] -= 1
             team[i + 1] += 1
             
+    # 최종적으로 체육복이 1개 이상 있는 사람의 수를 카운트
     for k, v in team.items() :
         if v >= 1 :
             count += 1
