7ace82dfb97bc91f59e82ee4c990b89d6f1ca855
JunHyeong Park <suldangoo@gmail.com>
2024-02-29 21:22:49 +0900

    solved problem

--- baekjoon/silver/1622. 공통 순열/공통 순열.py
+++ baekjoon/silver/1622. 공통 순열/공통 순열.py
@@ -1,17 +1,19 @@
 # 1622. 공통 순열 (실버 4)
 # 알고리즘 분류 : 문자열, 정렬
 
+# 종료 조건을 위해  try문 사용
 try :
     while True :
-        answer = set()
+        answer = list()
         a = input()
         b = input()
 
         for i in a :
-            if i in b :
-                answer.add(i)
+            if i in b : # 문자열 하나가 해당 문자열 안에 존재할 경우
+                answer += [i] # 답 리스트에 문자열 추가
+                b = b.replace(i, '', 1) # b 문자열에 남은 i들을 모두 제거
 
-        answer = sorted(list(answer))
+        answer = sorted(answer)
         print(''.join(answer))
 except :
     pass
