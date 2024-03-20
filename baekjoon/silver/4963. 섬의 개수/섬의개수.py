63e7280714a13efd6ee6e7f6a0750507dcd40e98
JunHyeong Park <suldangoo@gmail.com>
2024-03-20 23:37:31 +0900

    solved 4963

--- baekjoon/silver/4963. 섬의 개수/섬의개수.py
+++ baekjoon/silver/4963. 섬의 개수/섬의개수.py
@@ -3,23 +3,25 @@
 
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
 # 위치 정보 변수 (가로 세로 대각선)
 dx = [1, -1, 0, 0, 1, -1, 1, -1]
 dy = [0, 0, 1, -1, 1, -1, -1, 1]
 
-def dfs(x, y) :
-    for i in range(8) :
-        ddx = x + dx[i]
-        ddy = y + dy[i]
-
-        if ddx < 0 or ddx >= w or ddy < 0 or ddy >= h :
-            return False
+def dfs(y, x) :
+    if x < 0 or x >= w or y < 0 or y >= h :
+        return False
         
-        if graph[y][x] == 1:
-            graph[y][x] = 0
+    if graph[y][x] == 1:
+        graph[y][x] = 0
 
+        for i in range(8) :
+            ddx = x + dx[i]
+            ddy = y + dy[i]
 
+            dfs(ddy, ddx)
+        return True
     return False
 
 while True :
@@ -36,7 +38,7 @@ while True :
 
     for i in range(h) :
         for j in range(w) : 
-            if dfs(j, i) == True :
+            if dfs(i, j) == True :
                 result += 1
 
     print(result)
