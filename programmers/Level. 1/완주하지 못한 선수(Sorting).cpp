d5075b9c614f420bb55735952cdb92f013e3a440
JunHyeong Park <suldangoo@gmail.com>
2024-06-06 18:53:16 +0900

    solved lv.1

---
+++ programmers/Level. 1/완주하지 못한 선수(Sorting).cpp
@@ -0,0 +1,22 @@
+// Sorting / Loop를 활용
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    // 1. 두 배열을 정렬한다.
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    
+    // 2. 두 배열이 다를 때까지찾는다.
+    int i = 0;
+    for (; i < completion.size(); i++)
+        if (participant[i] != completion[i])
+            break;
+    
+    // 3. 마지막까지 완주하지 못한 선수를 리턴한다.
+    return participant[i];
+}
