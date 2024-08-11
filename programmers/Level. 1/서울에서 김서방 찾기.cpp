41b8c7cb6e278be2747a25d167e65a2899c9d20d
JunHyeong Park <suldangoo@gmail.com>
2024-08-12 00:12:32 +0900

    solved lv.1

---
+++ programmers/Level. 1/서울에서 김서방 찾기.cpp
@@ -0,0 +1,14 @@
+// 프로그래머스 Lv.1 서울에서 김서방 찾기
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    for (int i = 0; i < seoul.size(); i++) {
+        if (seoul[i] == "Kim") {
+            return "김서방은 " + to_string(i) + "에 있다";
+        }
+    }
+}
