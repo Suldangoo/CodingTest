1cfb2997167bb63d867133b417c8520dc15e2a85
Suldangoo <suldangoo@gmail.com>
2025-05-22 23:33:45 +0900

    코드 수정

--- programmers/Level. 0/문자 반복 출력하기.cpp
+++ programmers/Level. 0/문자 반복 출력하기.cpp
@@ -5,12 +5,13 @@
 
 using namespace std;
 
-string solution(string my_string, int n) {
+string solution(string my_string, int n)
+{
     string answer = "";
-    
+
     for (int i = 0; i < my_string.size(); i++)
         for (int j = 0; j < n; j++)
             answer += my_string[i];
-    
+
     return answer;
 }
